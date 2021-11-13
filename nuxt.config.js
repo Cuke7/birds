16c3b231eba6f8d2d1d10c24ea36e49711de4789
Louis <cuke.dev@gmail.com>
2021-11-13 20:02:43 +0100

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,76 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: "%s - birds",
+    title: "birds",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify",
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    "@nuxtjs/axios",
+    // https://go.nuxtjs.dev/pwa
+    "@nuxtjs/pwa",
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: "en",
+    },
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          red: "#ff5555",
+          yellow: "#f1fa8c",
+          purple: "#be93f9",
+          pink: "#ff79c6",
+          orange: "#ffb86c",
+          green: "#50fa7b",
+          cyan: "#8be9fd",
+          background2: "#383a59",
+          foreground: "#f8f8f2",
+          selection: "#44475a",
+          background: "#282a36",
+          grey: "#bcc2cd",
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row justify="center" align="start">
+    <v-col cols="12" sm="8" md="6">
+      <h1>Hello Dracula</h1>
+    </v-col>
+  </v-row>
+</template>
