c4f64a8bf3f03d3bfc77d276a497ea4d0454bfac
Cuke7 <cuke.dev@mail.com>
2021-11-16 15:23:26 +0100

    nuxt stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from "vuetify/es5/util/colors";
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -86,6 +84,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-
-  target: "static",
 };
