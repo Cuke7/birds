6cfb52fce942391b37d2ba84450c46e4e4768639
Cuke7 <cuke.dev@mail.com>
2021-11-16 10:59:45 +0100

    Searching

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,50 @@
 <template>
   <v-row justify="center" align="start">
     <v-col cols="12" md="6">
-      <Bird :bird="birdTest"></Bird>
+      <!-- <Bird :bird="birdTest"></Bird> -->
+
+      <v-text-field
+        color="purple"
+        label="Chercher un oiseau"
+        v-model="searchQuery"
+        autocomplete="off"
+        class="ma-6"
+      ></v-text-field>
+
+      <div v-for="(searchResult, index) in searchResults" :key="index">
+        {{ searchResult.name }}
+      </div>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import birdData from "~/static/birdData.json";
+const { Searcher } = require("fast-fuzzy");
 export default {
-  data: () => ({}),
+  data: () => ({
+    searchQuery: "",
+    searchResults: [],
+  }),
   computed: {
     birdTest() {
       return this.$store.getters.getBirdData("Rougegorge familier");
     },
+    searcher() {
+      console.log("Computing");
+      return new Searcher(birdData, {
+        keySelector: (obj) => obj.name,
+      });
+    },
+  },
+  watch: {
+    searchQuery(val) {
+      this.searchResults = this.searcher
+        .search(val, {
+          returnMatchData: true,
+        })
+        .map((el) => el.item);
+    },
   },
 };
 </script>
-
-<style scoped>
-h4 {
-  word-break: break-word;
-}
-</style>
