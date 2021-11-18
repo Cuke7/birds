9e78f0298e4aa9f14145c1e7f6d86abf23aa0548
Cuke7 <cuke.dev@mail.com>
2021-11-18 18:15:33 +0100

    removed share

--- layouts/subpage.vue
+++ layouts/subpage.vue
@@ -7,9 +7,9 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon @click="share">
+      <!-- <v-btn icon @click="share">
         <v-icon color="green"> mdi-share-variant </v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
     <v-main>
       <v-container>
