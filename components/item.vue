84137cd624395079453a6ec5403ecae800154998
Cuke7 <cuke.dev@mail.com>
2022-01-12 13:39:05 +0100

    test

---
+++ components/item.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="pa-0" height="100%">
+    <v-img :src="scope.data.item.images[0].hdLink" max-height="80%" contain></v-img>
+
+    <v-row justify="center">
+      <v-col cols="auto">
+        <v-card-title class="white--text">
+          {{ scope.data.item.name }}
+        </v-card-title>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["scope"],
+};
+</script>
+
+<style scoped>
+.v-card {
+  background-image: linear-gradient(
+    to left bottom,
+    #0056d8,
+    #006ac3,
+    #00687c,
+    #005e35,
+    #3a4f0b
+  );
+}
+</style>
---
+++ layouts/simple.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <v-app-bar app color="background">
+      <v-btn icon @click="$router.push('/')">
+        <v-icon color="green"> mdi-arrow-left </v-icon>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid style="height: 100vh">
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>
+
+<style scoped>
+.v-application {
+  background-color: #282a36;
+}
+</style>
