65bfa74d5988d27ba90a338df8a21cd33b5afb3e
Cuke7 <cuke.dev@mail.com>
2022-01-12 12:51:27 +0100

    bird tinder

---
+++ pages/birder.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="start">
+    <v-col cols="12" md="6"> </v-col>
+  </v-row>
+</template>
+
+<script>
+import birdData from "~/static/birdData.json";
+
+export default {
+  mounted() {
+    //
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    //
+  },
+};
+</script>
